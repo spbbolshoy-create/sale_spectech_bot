282a7381433e69b912a2e81657ad3ff3ee805cd0
spbbolshoy-create <spb.bolshoy@gmail.com>
2025-11-19 12:17:15 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
 BOT_TOKEN = "8326562946:AAH1x7pun9nxmB76248y3sx2BJYZ3Wqcolo"
-ADMIN_IDS = [316111351]  # Ваш ID в Telegram
-ADMIN_USERNAME = "bolshoyspb"  # Без @, например "ivan_ivanov"
+ADMIN_IDS = [316111351, 6896588835]  # Ваш ID в Telegram
+ADMIN_USERNAMES = ["bolshoyspb", "fergisons"]  # Без @, например "ivan_ivanov"
